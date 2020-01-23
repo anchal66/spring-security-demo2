6941c91d746be396de79d2bff49700fa15448a6b
Avinash Anchal <you@example.comanchalavinash66@gmail.com>
2020-01-23 08:46:51 +0530

    Configure Spring security

---
+++ src/main/java/com/anchal/springsecurity/config/DemoSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.anchal.springsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //add users for n memory authentication
+
+        UserBuilder users = User.withDefaultPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser(users.username("john").password("test").roles("EMPLOYEE"))
+                .withUser(users.username("avi").password("test").roles("MANAGER"))
+                .withUser(users.username("anchal").password("test").roles("ADMINs"));
+    }
+}
---
+++ src/main/java/com/anchal/springsecurity/config/SecurityWebApplicationInitialer.java
@@ -0,0 +1,7 @@
+package com.anchal.springsecurity.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitialer extends AbstractSecurityWebApplicationInitializer {
+
+}
